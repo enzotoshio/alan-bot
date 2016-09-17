56098771836d07a3194ca1ba6dcf456a4b5efc0f
Enzo Toshio <enzo.toshiba@gmail.com>
2016-09-17 11:02:30 -0300

    adding route response so heroku doesn crash

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const OpinionController = require('./controllers/opinion.controller');
 const DumbAlanController = require('./controllers/dumb-alan.controller');
 const StartController = require('./controllers/start.controller');
 const StopController = require('./controllers/stop.controller');
-const token = '273431119:AAEhnEY5n-IEPS7cl_TPQcAlH18Jo0jHE70';
+const request = require('request');
+const server = require('node-router').getServer();
 
+const token = '273431119:AAEhnEY5n-IEPS7cl_TPQcAlH18Jo0jHE70';
 const TelegramBaseController = Telegram.TelegramBaseController;
 const tg = new Telegram.Telegram(token);
 
 let channel = {};
+
+
+server.get("/", function (request, response) {
+  response.simpleText(200, "Hello World!");
+});
+
+// setInterval(() => {
+//
+// }, 1740000);
 
 tg.router
     .when('/start', new StartController(channel))
